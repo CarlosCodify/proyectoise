a6ec96362a550fac40d5d9f8bdc480da21c90395
carloscodify <carlos.gutierrez@codify.com.bo>
2023-03-21 16:45:49 -0400

    Modify cors

---
+++ app/controllers/api/v1/bancos_controller.rb
@@ -0,0 +1,51 @@
+class Api::V1::BancosController < ApplicationController
+  before_action :set_banco, only: %i[ show update destroy ]
+
+  # GET /api/v1/bancos
+  def index
+    @bancos = Banco.all
+
+    render json: @bancos
+  end
+
+  # GET /api/v1/bancos/1
+  def show
+    render json: @banco
+  end
+
+  # POST /api/v1/bancos
+  def create
+    @banco = Banco.new(banco_params)
+
+    if @banco.save
+      render json: @banco, status: :created
+    else
+      render json: @banco.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /api/v1/bancos/1
+  def update
+    if @banco.update(banco_params)
+      render json: @banco
+    else
+      render json: @banco.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v1/bancos/1
+  def destroy
+    @banco.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_banco
+      @banco = Banco.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def banco_params
+      params.require(:banco).permit(:nombre)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :bancos
       resources :tipo_activo_fijos
       resources :sucursales do
         resources :activo_fijos, shallow: true
